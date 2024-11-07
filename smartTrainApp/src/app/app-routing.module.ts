d36c860f63e1c8d4cf5b85d14934ec8785c9e381
Balaji-064 <praveenbalaji433@gmail.com>
2024-11-07 17:41:45 +0530

    7-11-24 Create a SmartTrainApp

---
+++ smartTrainApp/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { BookingsComponent } from './pages/bookings/bookings.component';
+import { AdminComponent } from './pages/admin/admin.component';
+
+const routes: Routes = [
+
+
+  {path:'',component:HomeComponent},
+  {path:'search',component:SearchComponent},
+  {path:'bookings',component:BookingsComponent},
+  {path:'admin',component:AdminComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ smartTrainApp/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { BookingsComponent } from './pages/bookings/bookings.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { HttpClientModule } from '@angular/common/http'
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    BookingsComponent,
+    AdminComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
